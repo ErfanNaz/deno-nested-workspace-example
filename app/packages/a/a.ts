18416dc12c0361325b5ce7b2bc9a9a127a9b1027
Erfan Nazmehr <erfan.nazm@gmail.com>
2025-05-19 18:20:50 +0200

    added example

---
+++ app/packages/a/a.ts
@@ -0,0 +1,6 @@
+import { core } from '@example/core' // <- is not working
+import { b } from '@example/app/b' // <- or @example/b is not working
+
+export function a() {
+  console.log("hallo a")
+}
